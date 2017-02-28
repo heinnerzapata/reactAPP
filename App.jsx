104fe46d1f481dcd01f36a02accd4de7b1b54231
heinner.zapata <heinnerzapata1991@gmail.com>
2017-02-28 00:23:36 -0500

    rect component life cycle

--- App.jsx
+++ App.jsx
@@ -1,67 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-   constructor() {
+
+  constructor() {
       super();
+      this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+   };
 
-      this.state = {
-         data:
-         [
-            {
-               "id":1,
-               "name":"Foo",
-               "age":"20"
-            },
-
-            {
-               "id":2,
-               "name":"Bar",
-               "age":"30"
-            },
-
-            {
-               "id":3,
-               "name":"Baz",
-               "age":"40"
-            }
-         ]
-      }
+   findDomNodeHandler() {
+      var myDiv = document.getElementById('myDiv');
+      ReactDOM.findDOMNode(myDiv).style.color = 'green';
    }
 
    render() {
       return (
-         <div>
-            <Header/>
-            <table>
-               <tbody>
-                  {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-               </tbody>
-            </table>
-         </div>
+
+        <div>
+        <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
+           <div id = "myDiv">NODE</div>
+        </div>
+
       );
    }
 }
 
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
-}
-
-class TableRow extends React.Component {
-   render() {
-      return (
-         <tr>
-            <td>{this.props.data.id}</td>
-            <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
-         </tr>
-      );
-   }
-}
 
 export default App;
